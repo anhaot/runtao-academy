a349ef1e7d06643e01b18f7f7e6d31260003bfce
anhao <1073663999@qq.com>
2026-07-27 11:55:20 +0800

    test: serialize stateful e2e suite

--- web/playwright.config.ts
+++ web/playwright.config.ts
@@ -13,6 +13,7 @@ export default defineConfig({
     timeout: 10_000,
   },
   fullyParallel: false,
+  workers: 1,
   reporter: [['list'], ['html', { open: 'never' }]],
   use: {
     baseURL: `http://127.0.0.1:${webPort}`,
